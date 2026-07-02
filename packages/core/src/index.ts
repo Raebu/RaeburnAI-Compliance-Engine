cd3eece7f90d03a847a43dc6bb86bd0eec68e736
The Raeburn Group <142256348+Raebu@users.noreply.github.com>
2026-07-02 02:05:25 +0100

    Export core APIs

---
+++ packages/core/src/index.ts
@@ -0,0 +1,4 @@
+export { assessSystem, buildRemediationRoadmap, classifyRisk } from './engine.js';
+export { rules } from './rules.js';
+export { AISystemSchema, FrameworkSchema, RiskLevelSchema } from './types.js';
+export type { AISystem, AssessmentResult, ComplianceRule, Framework, RiskLevel, RuleResult, RuleSeverity } from './types.js';
